3e890feb838b8f1bd8ff293ce8ef81ce122610e6
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2026-02-09 17:05:06 +0100

    Updating storage from txt to JSON file

--- Password_Manager_and_Generator/model.py
+++ Password_Manager_and_Generator/model.py
@@ -1,8 +1,9 @@
 import os
 import string
 from random import choice
+import json
 
-DATA_FILE_PATH = "user_data.txt"
+DATA_FILE_PATH = "user_data.json"
 
 class Model:
     def __init__(self):
@@ -13,13 +14,23 @@ class Model:
     def create_data_file() -> None:
         if not os.path.exists(DATA_FILE_PATH):
             with open(DATA_FILE_PATH, "w") as file:
-                file.write(f"Website | Email/Username | Password\n{"-"*35}\n")
+                json.dump({}, file)
 
     @staticmethod
-    def save_data_to_file(website:str, email_username:str, password:str) -> None:
-        with open(DATA_FILE_PATH, "a") as file:
-            file.write(f"{website} | {email_username} | {password}\n")
+    def save_data_to_file(website: str, email_username: str, password: str) -> None:
+        new_data = {
+            website: {
+                "email/username": email_username,
+                "password": password
+            }
+        }
+        with open(DATA_FILE_PATH, "r") as file:
+            data = json.load(file)
+            data.update(new_data)
 
-    def generate_password(self, number_of_characters:int) -> str:
-        password = "".join(choice(self.characters) for i in range(number_of_characters))
+        with open(DATA_FILE_PATH, "w") as file:
+            json.dump(obj=data, fp=file, indent=4)
+
+    def generate_password(self, number_of_characters: int) -> str:
+        password = "".join(choice(self.characters) for _ in range(number_of_characters))
         return password
