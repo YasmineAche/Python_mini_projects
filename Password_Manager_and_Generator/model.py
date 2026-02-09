619719cd56bcbad152f0ecfe399426cf383136ce
YasmineAche <128965544+YasmineAche@users.noreply.github.com>
2026-02-09 15:38:48 +0100

    Changing "data_file_path" from attribute to constant

--- Password_Manager_and_Generator/model.py
+++ Password_Manager_and_Generator/model.py
@@ -2,19 +2,22 @@ import os
 import string
 from random import choice
 
+DATA_FILE_PATH = "user_data.txt"
+
 class Model:
     def __init__(self):
-        self.data_file_path = "user_data.txt"
         self.characters = f"{string.ascii_letters}{string.digits}{string.punctuation}"
         self.create_data_file()
 
-    def create_data_file(self) -> None:
-        if not os.path.exists(self.data_file_path):
-            with open(self.data_file_path, "w") as file:
+    @staticmethod
+    def create_data_file() -> None:
+        if not os.path.exists(DATA_FILE_PATH):
+            with open(DATA_FILE_PATH, "w") as file:
                 file.write(f"Website | Email/Username | Password\n{"-"*35}\n")
 
-    def save_data_to_file(self, website:str, email_username:str, password:str) -> None:
-        with open(self.data_file_path, "a") as file:
+    @staticmethod
+    def save_data_to_file(website:str, email_username:str, password:str) -> None:
+        with open(DATA_FILE_PATH, "a") as file:
             file.write(f"{website} | {email_username} | {password}\n")
 
     def generate_password(self, number_of_characters:int) -> str:
